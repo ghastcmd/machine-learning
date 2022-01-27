00a291d0b94efdf0f9c4c721630782ed62651496
GhastCMD <kraken32bin@protonmail.com>
2022-01-27 18:04:24 -0300

    Made adjustments for better explanation of clustering's profiles

--- exercises/3 clustering/src/Clustering.py
+++ exercises/3 clustering/src/Clustering.py
@@ -1,9 +1,7 @@
 # %% The first cell
 import pandas as pd
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
-from tqdm import tqdm
 
 # %% opening excel
 df = pd.read_excel('../barrettII_eyes_clustering.xlsx')
@@ -14,71 +12,58 @@ print(df.describe())
 # N_CLUSTERS = 5
 COLUMN_VALUES = ['AL', 'ACD', 'WTW', 'K1', 'K2']
 MAX_CLUSTERS_TEST = 10
-# %% Normalizing the scale of values
-
-scaler = MinMaxScaler().fit(df[COLUMN_VALUES])
-scaled_df = df.copy()
-scaled_df[COLUMN_VALUES] = scaler.transform(scaled_df[COLUMN_VALUES])
 
 # %% To plot the elbow plot to check what is best
 
 k1 = []
 inertia_s1 = []
 
-for i in tqdm(range(2,21)):
+for i in range(2,21):
     k1.append(i)
-    kmeans1 = KMeans(n_clusters=i).fit(scaled_df[COLUMN_VALUES])
+    kmeans1 = KMeans(n_clusters=i).fit(df[COLUMN_VALUES])
     inertia_s1.append(kmeans1.inertia_)
 
 # plot
 plt.figure(figsize=(15,5))
-plt.plot(k1,inertia_s1,color='blue', linestyle='dashed', marker='o',
-         markerfacecolor='red', markersize=10)
-plt.title('Inertia (SSE) vs. K Value')
-plt.xlabel('K')
-plt.ylabel('Inertia score (SSE)')
+plt.plot(k1,inertia_s1)
+plt.title('Inércia vs. K')
+plt.xlabel('Valor de K')
+plt.ylabel('Inércia')
     
 # %% To use the best K
 
-kmeans = KMeans(n_clusters=5, random_state=123, max_iter=100).fit(scaled_df[COLUMN_VALUES])
+MAX_CLUSTERS = 4
+
+kmeans = KMeans(n_clusters=MAX_CLUSTERS, random_state=123, max_iter=100).fit(df[COLUMN_VALUES])
 
 df_features = df.copy()
 df_features['cluster_ids'] = kmeans.labels_
 df_features.head()
 
-# %% Analysing the data
-df_profile_overall = df_features.describe().drop('ID', axis=1).T
-df_profile_overall['Overall Dataset'] = df_profile_overall[['mean']]
-df_profile_overall = df_profile_overall[['Overall Dataset']]
+# %% This is to run the porfiling on the centroids
 
-df_cluster_summary = df_features.groupby('cluster_ids').describe().drop('ID', axis=1).T.reset_index()
-df_cluster_summary = df_cluster_summary.rename(columns={'level_0': 'column', 'level_1': 'metric'})
+kmeans_centroids = kmeans.cluster_centers_
 
-df_cluster_summary = df_cluster_summary[df_cluster_summary['metric'] == 'mean']
-df_cluster_summary = df_cluster_summary.set_index('column')
+print(kmeans_centroids)
 
-df_profile = df_cluster_summary.join(df_profile_overall)
+print('')
 
-df_profile
-# %%
-prof_w_count = df_profile.copy()
+centroids_df = pd.DataFrame()
+for i, column in enumerate(COLUMN_VALUES):
+    column_of_centroids = []
+    for j in range(MAX_CLUSTERS):
+        column_of_centroids.append(kmeans_centroids[j][i])
+    
+    centroids_df[column] = column_of_centroids
 
-count_df = df_features[df_features['Correto'] == 'S'].groupby('cluster_ids').count()
-count_df = count_df[['Correto']].rename(columns={'Correto': 'Correto Count'}).T
-
-total_count_df = df_features.groupby('cluster_ids').count()
-total_count_df = total_count_df[['K2']].rename(columns={'K2': 'Total Count'}).T
-
-total_count_df['Overall Dataset'] = total_count_df.sum(axis=1)
-total_count_df['metric'] = 'count'
-
-count_df['Overall Dataset'] = count_df.sum(axis=1)
-count_df['metric'] = 'count'
-
-prof_w_count = pd.concat([prof_w_count, count_df, total_count_df])
-prof_w_count
-
+centroids_df.head().T
 
 # %%
-prof_w_count
-prof_w_count.to_csv('../clustering_olhos.csv')
+
+correct_df = df_features[df_features['Correto'] == 'S'].groupby('cluster_ids')
+correct_df = correct_df.count()[['Correto']].T.rename({'Correto': 'Correto Count'})
+
+result_df = pd.concat([centroids_df.T, correct_df])
+result_df
+# %%
+result_df.to_csv('../clustering_olhos.csv')
